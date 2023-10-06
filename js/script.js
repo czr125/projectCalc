35669ad1291a05e207d4e54b2d51b19ca84c7629
czr_dev <cezar.holanda@outlook.com>
2023-10-06 17:22:02 -0300

    finished calculator

--- js/script.js
+++ js/script.js
@@ -1,187 +1,154 @@
-const previousOperationText = document.querySelector("#previous-operation");
-const currentOperationText = document.querySelector("#current-operation");
-const buttons = document.querySelectorAll("#buttons-container button");
-
-class Calculator {
-    constructor(previousOperationText, currentOperationText) {
-        this.previousOperationText = previousOperationText  // Printed value on screen
-        this.currentOperationText = currentOperationText   // Printed value on screen
-        this.currentOperation = ""  // User' value insert at the moment
-    }
-
-
-    // Add digit to calculator screen
-    addDigit(digit) {
-        // Check if current operation already has a dot
-        if(digit === "." && this.currentOperationText.innerText.includes(".")) {
-            return
-        }
-
-        this.currentOperation = digit
-        this.updateScreen()
-    }
-
-    // Process all calculator operations 
-    processOperation (operation){
-        // Check if current value is empty
-        if(this.currentOperationText.innerText === "" && operation != "C") {
-            // Change operation
-            if(this.previousOperationText.innerText !== "") {
-                this.changeOperation(operation)
-            }
-            return
-        }
-
-        // Get current and previous value
-        let operationValue
-        const previous = +this.previousOperationText.innerText.split(" ")[0]
-        const current = +this.currentOperationText.innerText
-
-        switch (operation) {
-            case "+":
-                operationValue = previous + current
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "-":
-                operationValue = previous - current
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "/":
-                operationValue = previous / current
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "*":
-                operationValue = previous * current
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "**":
-                function exponenciacaoAoQuadrado(current) {
-                    return Math.pow(current, 2);
-                    }
-                operationValue = exponenciacaoAoQuadrado(current)
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "**n":
-                function elevadoAEnesimaPotencia(previous, current) {
-                    return Math.pow(previous, current);
-                    }
-                operationValue = elevadoAEnesimaPotencia(previous, current)
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "x!":
-                function fatorial(current) {
-                    let fatorial = 1;
-                    for (let i = 2; i <= current; i++) {
-                      fatorial *= i;
-                    }
-                    return fatorial;
-                    }
-                operationValue = fatorial(current)
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "log2":
-                function logBase2(current) {
-                    return Math.log2(current);
-                    }
-                operationValue = logBase2()
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "log10":
-                function logBase10(current) {
-                    return Math.log10(current);
-                    }
-                operationValue = logBase10()
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "logn":
-                function logBaseN(previous,current) {
-                    return Math.log(previous) / Math.log(current);
-                    }
-                operationValue = logBaseN()
-                this.updateScreen(operationValue, operation, current, previous)
-                break
-            case "DEL":
-                this.processDelOperator()
-                break
-            case "CE":
-                this.processClearCurrentOperation()
-                break
-            case "C":
-                this.processClearAllOperation()
-                break
-            case "=":
-                this.processEqualOperator()
-                break
-            default:
-                return
-        }
-    
-    }
-
-    // Change values of the calculator screen
-    updateScreen(
-        operationValue = null, 
-        operation = null, 
-        current = null, 
-        previous = null
-    ) {
-        
-        if(operationValue === null) {
-            this.currentOperationText.innerText += this.currentOperation
-        } else {
-            // Check if value is 0, if it's just add current value
-            if(previous === 0){
-                operationValue = current
-            }
-            // Add current value to previous
-            this.previousOperationText.innerText = `${operationValue} ${operation}`
-            this.currentOperationText.innerText = ''
-        }
-    }
-    // Change math operation
-    changeOperation(operation) {
-        const mathOperations = ["*", "/", "+", "-", "**", "**n", "x!", "log2", "log10", "logn"]
-
-        if(!mathOperations.includes(operation)) {
-            return
-        }
-
-        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation
-    }
-
-    // Delete the last digit
-    processDelOperator(){
-        this.currentOperationText.innerText = this.currentOperationText.innerText.slice(0, -1)
-    }
-
-    // Clear current operation
-    processClearCurrentOperation(){
-        this.currentOperationText.innerText = ""
-    }
-
-    // Clear current and previous operation
-    processClearAllOperation(){
-        this.currentOperationText.innerText = ""
-        this.previousOperationText.innerText = ""
-    }
-
-    // Process an operation 
-    processEqualOperator(){
-        const operation = previousOperationText.innerText.split(" ")[1]
-
-        this.processOperation(operation)
-    }
+function factorialize(firstNum) {
+  if (firstNum < 0) 
+        return -1;
+  else if (firstNum == 0) 
+      return 1;
+  else {
+      return (firstNum * factorialize(firstNum - 1));
+  }
 }
 
-const calc = new Calculator(previousOperationText, currentOperationText)
-buttons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-        const value = e.target.innerText
+function logaritmize(firstNum, secondNum) {
+  return Math.log(firstNum) / Math.log(secondNum);
+}
 
-        if(+value >= 0 || value === '.') {
-            calc.addDigit(value)
-        } else {
-            calc.processOperation(value)
-        }
-    })
+const calculate = (n1, operator, n2) => {
+  const firstNum = parseFloat(n1)
+  const secondNum = parseFloat(n2)
+  if (operator === 'add') return firstNum + secondNum
+  if (operator === 'subtract') return firstNum - secondNum
+  if (operator === 'multiply') return firstNum * secondNum
+  if (operator === 'divide') return firstNum / secondNum
+  if (operator === 'exponencialByTwo') return firstNum ** 2
+  if (operator === 'exponencial') return firstNum ** secondNum
+  if (operator === 'factorial') return factorialize(firstNum)
+  if (operator === 'logaritmalTwo') return Math.log2(firstNum)
+  if (operator === 'logaritmalTen') return Math.log10(firstNum)
+  if (operator === 'logartimaln') return logaritmize(firstNum, secondNum)
+}
+
+const getKeyType = key => {
+  const { action } = key.dataset
+  if (!action) return 'number'
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide' ||
+    action === 'exponencialByTwo' ||
+    action === 'exponencial' ||
+    action === 'factorial' ||
+    action === 'logaritmalTwo' ||
+    action === 'logaritmalTen' ||
+    action === 'logaritmalN2' ||
+    action === 'logaritmalN10' 
+  ) return 'operator'
+  // For everything else, return the action
+  return action
+}
+
+const createResultString = (key, displayedNum, state) => {
+  const keyContent = key.textContent
+  const keyType = getKeyType(key)
+  const {
+    firstValue,
+    operator,
+    modValue,
+    previousKeyType
+  } = state
+
+  if (keyType === 'number') {
+    return displayedNum === '0' ||
+      previousKeyType === 'operator' ||
+      previousKeyType === 'calculate'
+      ? keyContent
+      : displayedNum + keyContent
+  }
+
+  if (keyType === 'decimal') {
+    if (!displayedNum.includes('.')) return displayedNum + '.'
+    if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.'
+    return displayedNum
+  }
+
+  if (keyType === 'operator') {
+    return firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
+
+  if (keyType === 'clear') return 0
+
+  if (keyType === 'calculate') {
+    return firstValue
+      ? previousKeyType === 'calculate'
+        ? calculate(displayedNum, operator, modValue)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
+}
+
+const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+  const keyType = getKeyType(key)
+  const {
+    firstValue,
+    operator,
+    modValue,
+    previousKeyType
+  } = calculator.dataset
+
+  calculator.dataset.previousKeyType = keyType
+
+  if (keyType === 'operator') {
+    calculator.dataset.operator = key.dataset.action
+    calculator.dataset.firstValue = firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+      ? calculatedValue
+      : displayedNum
+  }
+
+  if (keyType === 'calculate') {
+    calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
+      ? modValue
+      : displayedNum
+  }
+
+  if (keyType === 'clear' && key.textContent === 'AC') {
+    calculator.dataset.firstValue = ''
+    calculator.dataset.modValue = ''
+    calculator.dataset.operator = ''
+    calculator.dataset.previousKeyType = ''
+  }
+}
+
+const updateVisualState = (key, calculator) => {
+  const keyType = getKeyType(key)
+  Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
+
+  if (keyType === 'operator') key.classList.add('is-depressed')
+  if (keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC'
+  if (keyType !== 'clear') {
+    const clearButton = calculator.querySelector('[data-action=clear]')
+    clearButton.textContent = 'CE'
+  }
+}
+
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('.calculator__display')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+  if (!e.target.matches('button')) return
+  const key = e.target
+  const displayedNum = display.textContent
+  const resultString = createResultString(key, displayedNum, calculator.dataset)
+
+  display.textContent = resultString
+  updateCalculatorState(key, calculator, resultString, displayedNum)
+  updateVisualState(key, calculator)
 })
